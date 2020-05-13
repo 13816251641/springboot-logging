6e6424966a9caf63ba56640ffff7f0c5b06eb306
181268 <lujieni1017>
2020-05-13 11:31:59 +0800

    代码提交

--- src/main/java/com/lujieni/logging/controller/HelloController.java
+++ src/main/java/com/lujieni/logging/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.lujieni.logging.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2020/5/11
  */
 @RestController
+@Slf4j
 public class HelloController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
-
+   /*
+      不使用@Slf4j手工引入也可以,但都是org.slf4j包下的
+      private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    */
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
-        logger.info("hello");
+        log.info("hello");
         return "hello";
     }
 
