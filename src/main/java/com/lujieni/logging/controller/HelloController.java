0c755bd11bab0528d0fc262068d37fbf29ed4499
181268 <lujieni1017>
2021-08-20 11:47:14 +0800

    代码提交

--- src/main/java/com/lujieni/logging/controller/HelloController.java
+++ src/main/java/com/lujieni/logging/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import wahaha.Hello;
 
 /**
  * @Auther lujieni
@@ -19,12 +20,11 @@ public class HelloController {
       不使用@Slf4j手工引入也可以,但都是org.slf4j包下的
       private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     */
-
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
-        log.info("hello123");
+        Hello hello = new Hello();
+        hello.sayHello();
         return "hello";
     }
-
 
 }
